4a02190dca1f247647eb43f490f926d9a86c1b26
테라모션 <telamosyeon@telamosyeons-Mac-mini.local>
2024-05-14 15:53:00 +0900

    feat(payment): Portone을 통한 결제 연동 테스트

---
+++ portone/src/main/java/com/example/portone/filter/CorsFilter.java
@@ -0,0 +1,16 @@
+//package com.example.portone.filter;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//@Configuration
+//@EnableWebFlux
+//public class CorsFilter implements WebFluxConfigurer {
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*");
+//    }
+//}
---
+++ portone/src/main/java/com/example/portone/payment/controller/PaymentController.java
@@ -0,0 +1,42 @@
+package com.example.portone.payment.controller;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${iamport.key}")
+    private String restApiKey;
+    @Value("${iamport.secret}")
+    private String restApiSecret;
+
+    private IamportClient iamportClient;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(restApiKey, restApiSecret);
+    }
+
+    @PostMapping("/verifyIamport/{imp_uid}")
+    public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) throws IamportResponseException, IOException {
+        IamportResponse<Payment> payment = iamportClient.paymentByImpUid(imp_uid);
+        Payment paymentData = payment.getResponse();
+        log.info("결제 금액 : {}", paymentData.getAmount());
+        log.info("결제 요청 응답. 결제 내역 - 주문 번호 : {}", payment.getResponse());
+        return payment;
+    }
+}
